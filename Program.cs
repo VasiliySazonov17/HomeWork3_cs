e8da905bb4c7b799f832e141ea3a2cb7f120fd76
Vasiliy <vasiliyasazonov@gmail.com>
2022-06-15 22:27:43 +0300

    add program cube

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27.
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Input length row: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] row =new int[10];
+
+for (int i=0; i<length; i++)
+{
+    row[i] = (i+1)*(i+1)*(i+1);
+    Console.Write($"({i+1})^3 = {row[i]}");
+    if (i+1 < length)
+    {
+      Console.Write(", ");  
+    }
+    else if (i+1 == length)
+    {
+    Console.Write(". "); 
+    }
+    
+}
+
